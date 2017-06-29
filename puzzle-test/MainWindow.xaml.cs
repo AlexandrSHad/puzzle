e19c195918ec9aacb084cfc17a765d359aac6d3f
Alexandr SHad <shadurskij@gmail.com>
2017-06-29 15:04:57 +0300

    used grid. step 1.

--- puzzle-test/MainWindow.xaml.cs
+++ puzzle-test/MainWindow.xaml.cs
@@ -24,5 +24,52 @@ namespace puzzle_test
         {
             InitializeComponent();
         }
+
+        private void buttonMove_Click(object sender, RoutedEventArgs e)
+        {
+            img5.Source = new BitmapImage(new Uri("/img/6.png", UriKind.Relative));
+            img6.Source = new BitmapImage(new Uri("/img/5.png", UriKind.Relative));
+            //MessageBox.Show(img5.Source.BaseUri);
+        }
+
+        private void gridBoard_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                //MessageBox.Show("Drug");
+                Image img = (Image)e.Source;
+                
+                if (img.Source is BitmapImage)
+                {
+                    MessageBox.Show("BitmapImage");
+                    return;
+                }
+
+                //if (img.Source is BitmapFrame)
+                //{
+                //    MessageBox.Show("BitmapFrame");
+                //}
+
+                BitmapFrame bmp = (BitmapFrame)img.Source;
+                string uriString = bmp.Decoder.ToString();
+
+                DataObject drugData = new DataObject(uriString);
+
+                DragDrop.DoDragDrop(img, drugData, DragDropEffects.Move);
+            }
+        }
+
+        private void gridBoard_Drop(object sender, DragEventArgs e)
+        {
+            Image img = (Image)e.Source;
+
+            string uriString = (string)e.Data.GetData(typeof(string));
+
+            img.Source = new BitmapImage(new Uri(uriString));
+        }
     }
+
+    //Grid.SetRow(newImage, ColumnCount);
+    //Grid.SetColumn(newImage, ColumnCount);
+    //grid_Main.Children.Add(newImage);
 }
