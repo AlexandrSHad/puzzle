bab93922dcb752201df299eae11cc3815f6b9b88
Alexandr SHad <shadurskij@gmail.com>
2017-07-06 19:43:15 +0300

    move Tag class into Tag.cs

--- puzzle-test/MainWindow.xaml.cs
+++ puzzle-test/MainWindow.xaml.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -257,43 +256,5 @@ namespace puzzle_test
         //{
         //    PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
         //}
-    }
-
-    public class Tag : INotifyPropertyChanged
-    {
-        int _currentPosition;
-        public string ImagePath { get; set; }
-        public int CurrentPosition {
-            get { return _currentPosition; }
-            set
-            {
-                _currentPosition = value;
-                NotifyPropertyChanged("currentRow");
-                NotifyPropertyChanged("currentColumn");
-            }
-        }
-        public int RequiredPosition { get; set; }
-
-        public int CurrentRow
-        {
-            get
-            {
-                return _currentPosition / 3;
-            }
-        }
-        public int CurrentColumn
-        {
-            get
-            {
-                return _currentPosition % 3;
-            }
-        }
-
-        public event PropertyChangedEventHandler PropertyChanged = delegate { };
-
-        protected void NotifyPropertyChanged(string propertyName)
-        {
-            PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
-        }
     }
 }
