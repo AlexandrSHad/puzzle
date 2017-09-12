87ca3fc52cdcf9f78b0a9eadc38a28ee68bde832
Alexandr SHad <shadurskij@gmail.com>
2017-09-12 15:45:32 +0300

    #12 deleted unnessesary using directive

--- puzzle-test/HeapViewModel.cs
+++ puzzle-test/HeapViewModel.cs
@@ -1,11 +1,5 @@
 ﻿using System;
-using System.Collections;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
 
 namespace puzzle_test
 {
